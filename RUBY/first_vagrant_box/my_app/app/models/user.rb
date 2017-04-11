4366e7bc840c61b927642a5bbfb52b946bf611c1
Andy <andy07nguyen@gmail.com>
2017-04-11 10:30:22 -0700

    Ruby: update

---
+++ RUBY/first_vagrant_box/my_app/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_many :messages
+  # has_one :address
+
+  # def messages
+  #   User.find_by_sql("SELECT * FROM messages WHERE messages.user_id = #{self.id}")
+  # end
+end
